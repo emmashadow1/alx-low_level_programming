309cdde37ee6786c6b84c38ea5e628053e82ae27
NWABUEZE EMMANUEL <myschooltopic@gmail.com>
2022-09-08 14:31:18 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ *
+ *  * main - display this message "Programming is like building a multilingual puzzle
+ *
+ *   * Return: 0 if string works 
+ *
+ *    */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+}
