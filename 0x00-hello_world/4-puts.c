b6e7761a461560de893503174b45cca43e8d5a06
NWABUEZE EMMANUEL <myschooltopic@gmail.com>
2022-09-08 15:38:00 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,12 @@
 
 
 /**
- *
- *  * main - display this message "Programming is like building a multilingual puzzle
- *
- *   * Return: 0 if string works 
- *
- *    */
-
+* main - print the string in the put function
+*
+* Description: using the main function
+* this prints "Programming is like building a multilingual puzzle
+* Return 0
+*/
 int main(void)
 
 {
