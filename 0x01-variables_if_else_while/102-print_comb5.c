cce8150deca9e5e48671d641d3246d03756290b7
NWABUEZE EMMANUEL <myschooltopic@gmail.com>
2022-09-09 16:55:00 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,19 +7,14 @@
 int main(void)
 
 {
-
 	int i, j;
-
-
 
 	for (i = 0; i < 100; i++)
 
 	{
-
 		for (j = 0; j < 100; j++)
 
 		{
-
 			if (i < j)
 
 			{
@@ -37,10 +32,8 @@ int main(void)
 				if  (i != 98 || j != 99)
 
 				{
-
 					putchar(',');
-
-					putchar(' ');
+				putchar(' ');
 
 				}
 
