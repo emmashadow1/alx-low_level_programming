443c0353831f13fe86139d8a602008802e42d631
NWABUEZE EMMANUEL <myschooltopic@gmail.com>
2022-09-13 14:14:37 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,7 +6,6 @@
 void times_table(void)
 {
 	int i, j, k;
-
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
@@ -16,14 +15,12 @@ void times_table(void)
 			{
 				_putchar(k + '0');
 			}
-		if (k < 10 && j != 0)
-			{
+		if (k < 10 && j != 0)			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
 			} else if (k >= 10)
-
 			{
 				_putchar(',');
 				_putchar(' ');
