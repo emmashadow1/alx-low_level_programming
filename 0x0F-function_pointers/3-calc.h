e084b49d079d2de7cabda26a5f7ceab47677ab3e
NWABUEZE EMMANUEL <myschooltopic@gmail.com>
2022-10-12 15:42:15 -0700

    3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,27 +1,32 @@
-#ifndef CALC_H
-#define CALC_H
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#include "3-calc.h"
 /**
- * struct op - Struct op
+ *op_add - return difference of 2 numbers.
+ * @a: integer.
  *
- * @op: The operator
- * @f: The function associated
+ * Return: sum.
+ * operator given as a parameter
  */
-typedef struct op
+int (*get_op_func(char *s))(int, int)
 {
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
+	i = 0;
 
-#endif
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
